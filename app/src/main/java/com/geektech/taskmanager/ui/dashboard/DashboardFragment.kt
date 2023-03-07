9467e9c317ecbf3668e0d725182d93c8da0bf6db
Dilnaz27 <dbaudunova@gmail.com>
2023-03-07 23:11:55 +0600

    change edittext (hw 7)

--- app/src/main/java/com/geektech/taskmanager/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/geektech/taskmanager/ui/dashboard/DashboardFragment.kt
@@ -1,20 +1,25 @@
 package com.geektech.taskmanager.ui.dashboard
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import com.geektech.taskmanager.R
 import com.geektech.taskmanager.databinding.FragmentDashboardBinding
+import com.geektech.taskmanager.model.Quote
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.ktx.Firebase
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
+    private lateinit var db: FirebaseFirestore
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -23,8 +28,39 @@ class DashboardFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        db = FirebaseFirestore.getInstance()
+
+        binding.btnSave.setOnClickListener {
+            if (binding.etAuthor.text?.isNotEmpty()!! && binding.etQuote.text?.isNotEmpty()!!) {
+                save()
+            } else if (binding.etAuthor.text!!.isEmpty()) {
+                binding.etAuthor.error = getString(R.string.error_empty_value)
+            } else if (binding.etQuote.text?.isEmpty() == true) {
+                binding.etQuote.error = getString(R.string.error_empty_value)
+            } else {
+                binding.etAuthor.error = getString(R.string.error_empty_value)
+                binding.etQuote.error = getString(R.string.error_empty_value)
+            }
+        }
+    }
+
+    private fun save() {
+        val quote = Quote(
+            text = binding.etQuote.text.toString(),
+            author = binding.etAuthor.text.toString()
+        )
+        db.collection(FirebaseAuth.getInstance().currentUser?.uid.toString()).add(quote).addOnSuccessListener {
+            Toast.makeText(requireContext(),"Безумно можно быть первым!!!", Toast.LENGTH_SHORT).show()
+            binding.etQuote.text!!.clear()
+            binding.etAuthor.text!!.clear()
+        }.addOnFailureListener{
+            Log.e("di", "save error: " + it.message )
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/geektech/taskmanager/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/geektech/taskmanager/ui/notifications/NotificationsFragment.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.geektech.taskmanager.databinding.FragmentNotificationsBinding
+import com.geektech.taskmanager.model.Quote
+import com.geektech.taskmanager.ui.notifications.adapter.QuoteAdapter
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var db: FirebaseFirestore
+    private val adapter = QuoteAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,8 +24,20 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        return root
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        db = FirebaseFirestore.getInstance()
+        binding.recyclerView.adapter = adapter
+        db.collection(FirebaseAuth.getInstance().currentUser?.uid.toString()).get()
+            .addOnSuccessListener {
+                val data = it.toObjects(Quote::class.java)
+                adapter.addQuote(data)
+            }.addOnFailureListener {
+
+            }
     }
 
     override fun onDestroyView() {
